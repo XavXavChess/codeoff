3e51d0476a3f5f7e8b10abd055d52cce3601afb9
Xavier.D <167911556+XavXavChess@users.noreply.github.com>
2024-11-08 15:40:11 -1000

    null

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+
+
+let a = prompt("First digit code");
+let b = prompt("Second digit code");
+let c = prompt("Third digit code");
+let d = prompt("Forth digit code");
+
+//The encription part//
+let na = Math.floor(Math.random() * 100) + 1
+
 document.getElementById("demo").innerHTML =
-Math.floor(Math.random() * 10);
+na;
